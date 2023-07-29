d5cda40f7f7e15950d0e0e99ef044a0a77367959
SrishtiHasijani27 <Srishti310889@gmail.com>
2023-07-29 23:58:07 +0100

    code changes to put emoji back

--- zerver/lib/translate.py
+++ zerver/lib/translate.py
@@ -1,22 +1,28 @@
 import re
+import emoji
 from translate import Translator
 
 
-def translate_message(message, target_language):
+def extract_emojis(text):
+    # Function to extract emojis from the text
+    return ''.join(c for c in text if c in emoji.UNICODE_EMOJI)
+
+
+def translate_message_with_exceptions(message, target_language):
     # Regular expression to identify links in the message
     link_pattern = r'http[s]?://\S+'
-    # Regular expression to identify emojis in the message
-    emoji_pattern = r':[^\s]+:'
 
     # Find all links in the message and replace them with placeholders
     links = re.findall(link_pattern, message)
     for link in links:
         message = message.replace(link, f'<link_placeholder_{links.index(link)}>')
 
-    # Find all emojis in the message and replace them with placeholders
-    emojis = re.findall(emoji_pattern, message)
-    for emoji in emojis:
-        message = message.replace(emoji, f'<emoji_placeholder_{emojis.index(emoji)}>')
+    # Extract emojis from the message
+    emojis = extract_emojis(message)
+
+    # Replace emojis with placeholders
+    for emoji_char in emojis:
+        message = message.replace(emoji_char, f'<emoji_placeholder_{emojis.index(emoji_char)}>')
 
     # Translate the message using the translate module
     translator = Translator(to_lang=target_language)
@@ -25,7 +31,12 @@ def translate_message(message, target_language):
     # Replace the placeholders with the original links and emojis
     for i, link in enumerate(links):
         translated_message = translated_message.replace(f'<link_placeholder_{i}>', link)
-    for i, emoji in enumerate(emojis):
-        translated_message = translated_message.replace(f'<emoji_placeholder_{i}>', emoji)
+
+    for emoji_char in emojis:
+        translated_message = translated_message.replace(f'<emoji_placeholder_{emojis.index(emoji_char)}>',
+                                                        emoji_char)
 
     return translated_message
+
+
+
