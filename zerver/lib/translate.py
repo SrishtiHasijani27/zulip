0a7cc9de06be7950ac217c87b26ca839656c2564
SrishtiHasijani27 <Srishti310889@gmail.com>
2023-07-29 22:03:50 +0100

    Translate.py changed to convert the message without converting emoji and links

--- zerver/lib/translate.py
+++ zerver/lib/translate.py
@@ -2,66 +2,33 @@ import re
 from translate import Translator
 
 
-def extract_links(text):
-    # Regular expression to identify links in the text
+def translate_message_with_exceptions(message, target_language):
+    # Regular expression to identify links in the message
     link_pattern = r'http[s]?://\S+'
-    return re.findall(link_pattern, text)
-
-
-def extract_emojis(text):
-    # Regular expression to identify emojis in the text
+    # Regular expression to identify emojis in the message
     emoji_pattern = r'[^\w\s,]'
-    return re.findall(emoji_pattern, text)
 
-
-def replace_links_and_emojis_with_placeholders(text, links, emojis):
-    # Replace links with placeholders
+    # Find all links in the message and replace them with placeholders
+    links = re.findall(link_pattern, message)
     for link in links:
-        text = text.replace(link, f'<link_placeholder_{links.index(link)}>')
+        message = message.replace(link, f'<link_placeholder_{links.index(link)}>')
 
-    # Replace emojis with placeholders
+    # Find all emojis in the message and replace them with placeholders
+    emojis = re.findall(emoji_pattern, message)
     for emoji in emojis:
-        text = text.replace(emoji, f'<emoji_placeholder_{emojis.index(emoji)}>')
+        message = message.replace(emoji, f'<emoji_placeholder_{emojis.index(emoji)}>')
 
-    return text
+    # Translate the message using the translate module
+    translator = Translator(to_lang=target_language)
+    translated_message = translator.translate(message)
 
-
-def restore_placeholders_with_links_and_emojis(translated_text, links, emojis):
-    # Replace placeholders with links
+    # Replace the placeholders with the original links and emojis
     for i, link in enumerate(links):
-        translated_text = translated_text.replace(f'<link_placeholder_{i}>', link)
-
-    # Replace placeholders with emojis
+        translated_message = translated_message.replace(f'<link_placeholder_{i}>', link)
     for i, emoji in enumerate(emojis):
-        translated_text = translated_text.replace(f'<emoji_placeholder_{i}>', emoji)
+        translated_message = translated_message.replace(f'<emoji_placeholder_{i}>', emoji)
 
-    return translated_text
+    return translated_message
 
 
-def translate_message(text, target_language_code):
-    """
-    Translates given text string to specified target language using chosen translation API.
-    :param text: Input Text String (str)
-    :param target_language_code: Target Language Code (e.g., 'es' for Spanish) (str)
-    :return translated_text_string: Returns Translated Text String (str)
-    """
 
-    # Extract links and emojis from the input text
-    links = extract_links(text)
-    emojis = extract_emojis(text)
-
-    # Replace links and emojis with placeholders
-    text_with_placeholders = replace_links_and_emojis_with_placeholders(text, links, emojis)
-
-    # Use The Chosen Translation API To Translate Message Text From Source To Target Language
-    translator = Translator(to_lang=target_language_code)
-
-    # Translate the text with placeholders using the specified target language code
-    translated_text_with_placeholders = translator.translate(text_with_placeholders)
-    print(f"translated_text_with_placeholders",translated_text_with_placeholders)
-
-    # Restore the original links and emojis in the translated text
-    translated_text = restore_placeholders_with_links_and_emojis(translated_text_with_placeholders, links,
-                                                                 emojis)
-
-    return translated_text
