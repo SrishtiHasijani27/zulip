0a75a504887dca16978cb9594106b7fbc3ae0733
SrishtiHasijani27 <Srishti310889@gmail.com>
2023-07-29 22:44:31 +0100

    Logger added to check the incoming message

--- zerver/lib/translate.py
+++ zerver/lib/translate.py
@@ -7,6 +7,7 @@ def translate_message(message, target_language):
     link_pattern = r'http[s]?://\S+'
     # Regular expression to identify emojis in the message
     emoji_pattern = r'[^\w\s,]'
+    print(f"Original message is ", message)
 
     # Find all links in the message and replace them with placeholders
     links = re.findall(link_pattern, message)
@@ -17,6 +18,7 @@ def translate_message(message, target_language):
     emojis = re.findall(emoji_pattern, message)
     for emoji in emojis:
         message = message.replace(emoji, '')
+        print(f"message after emoji is :",message)
 
     # Translate the message using the translate module
     translator = Translator(to_lang=target_language)
