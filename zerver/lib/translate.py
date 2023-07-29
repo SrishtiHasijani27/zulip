45f188e0c70e23202250d679a232d4908c226e5c
SrishtiHasijani27 <Srishti310889@gmail.com>
2023-07-30 00:53:15 +0100

    code changes to handle the emoji

--- zerver/lib/translate.py
+++ zerver/lib/translate.py
@@ -4,38 +4,22 @@ from translate import Translator
 
 
 def extract_emojis(text):
-    # Regular expression to identify emojis in the text
-    emoji_pattern = r'[^\u0000-\u007F]+'
-    return ''.join(c for c in text if re.match(emoji_pattern, c))
+    return ''.join(c for c in text if c in emoji.UNICODE_EMOJI)
 
 
-def translate_message(message, target_language):
-    # Regular expression to identify links in the message
-    link_pattern = r'http[s]?://\S+'
+def translate_message(text, target_language_code):
+    # Extract emojis from the original text
+    emojis = extract_emojis(text)
 
-    # Find all links in the message and replace them with placeholders
-    links = re.findall(link_pattern, message)
-    for link in links:
-        message = message.replace(link, f'<link_placeholder_{links.index(link)}>')
+    # Remove emojis from the text
+    text_without_emojis = ''.join(c for c in text if c not in emoji.UNICODE_EMOJI)
 
-    # Extract emojis from the message
-    emojis = extract_emojis(message)
+    # Translate the text without emojis
+    translator = Translator(to_lang=target_language_code)
+    translated_text = translator.translate(text_without_emojis)
 
-    # Replace emojis with placeholders
-    for emoji_char in emojis:
-        message = message.replace(emoji_char, f'<emoji_placeholder_{emojis.index(emoji_char)}>')
-
-    # Translate the message using the translate module
-    translator = Translator(to_lang=target_language)
-    translated_message = translator.translate(message)
-
-    # Replace the placeholders with the original links and emojis
-    for i, link in enumerate(links):
-        translated_message = translated_message.replace(f'<link_placeholder_{i}>', link)
-
-    for emoji_char in emojis:
-        translated_message = translated_message.replace(f'<emoji_placeholder_{emojis.index(emoji_char)}>',
-                                                        emoji_char)
+    # Reinsert emojis into the translated text
+    translated_message = ''.join(
+        [a + b if b in emoji.UNICODE_EMOJI else a for a, b in zip(translated_text, emojis)])
 
     return translated_message
-
