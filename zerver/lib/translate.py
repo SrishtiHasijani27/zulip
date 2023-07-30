df1c47aa0a308968cdf98f69234c5b19c74020eb
SrishtiHasijani27 <Srishti310889@gmail.com>
2023-07-30 01:29:38 +0100

    Library changed to Google translate library

--- zerver/lib/translate.py
+++ zerver/lib/translate.py
@@ -1,40 +1,34 @@
 import re
-import emoji as emoji_module
-from translate import Translator
-
+from googletrans import Translator
+import emoji
 
 def extract_emojis(text):
-    # Regular expression to identify emojis in the text
     emoji_pattern = r'[^\u0000-\u007F]+'
     return ''.join(c for c in text if re.match(emoji_pattern, c))
-
 
 def translate_message(message, target_language):
     # Regular expression to identify links in the message
     link_pattern = r'http[s]?://\S+'
-    # Regular expression to identify emojis in the message
-    emoji_pattern = r'[^\s]+'
 
     # Find all links in the message and replace them with placeholders
     links = re.findall(link_pattern, message)
     for link in links:
         message = message.replace(link, f'<link_placeholder_{links.index(link)}>')
 
-    # Find all emojis in the message and replace them with placeholders
-    emojis = re.findall(emoji_pattern, message)
-    for emoji in emojis:
-        message = message.replace(emoji, f'<emoji_placeholder_{emojis.index(emoji)}>')
+    # Extract and remove emojis from the message
+    emojis = extract_emojis(message)
+    message_without_emojis = ''.join(c for c in message if c not in emoji.UNICODE_EMOJI)
 
-    # Translate the message using the translate module
-    translator = Translator(to_lang=target_language)
-    translated_message = translator.translate(message)
+    # Translate the message without emojis using the Google Translate API
+    translator = Translator()
+    translated_message_without_emojis = translator.translate(message_without_emojis, dest=target_language).text
 
-    # Replace the placeholders with the original links and emojis
+    # Reinsert emojis back into the translated message
+    translated_message = ''.join([a + b if b in emoji.UNICODE_EMOJI else a for a, b in zip(translated_message_without_emojis, emojis)])
+
+    # Replace the placeholders with the original links
     for i, link in enumerate(links):
         translated_message = translated_message.replace(f'<link_placeholder_{i}>', link)
 
-    for i, emoji in enumerate(emojis):
-        translated_message = translated_message.replace(f'<emoji_placeholder_{i}>',
-                                                        emoji_module.emojize(emoji.lower()))
-
     return translated_message
+
