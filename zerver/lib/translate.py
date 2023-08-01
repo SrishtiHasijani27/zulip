8a124cd1723c6f90a91e2ccf90c2bb6b236e5b00
SrishtiHasijani27 <Srishti310889@gmail.com>
2023-08-01 12:25:13 +0100

    Exception handling done

--- zerver/lib/translate.py
+++ zerver/lib/translate.py
@@ -6,17 +6,14 @@ def extract_emojis(text):
     emoji_pattern = r'[^\u0000-\u007F]+'
     return ''.join(c for c in text if re.match(re.escape(emoji_pattern), c))
 
-
 def remove_links(message):
     link_pattern = r'http[s]?://\S+'
     return re.sub(link_pattern, '', message)
-
 
 def detect_source_language(message):
     detected_language, _ = langid.classify(message)
     print(f"Detected Language: {detected_language}")
     return detected_language
-
 
 def translate_message(message, target_language):
     # Detect the source language of the message
@@ -34,35 +31,36 @@ def translate_message(message, target_language):
     # Remove <p> tags from the message and replace them with placeholders
     message_without_p_tags = message_without_links.replace('<p>', '').replace('</p>', '<p_placeholder>')
 
-    # Translate the message without <p> tags using the translate module
-    translator = Translator(from_lang=source_language, to_lang=target_language)
+    try:
+        # Translate the message without <p> tags using the translate module
+        translator = Translator(from_lang=source_language, to_lang=target_language)
 
-    # Split the message into smaller chunks (max 500 characters each)
-    chunk_size = 500
-    chunks = [message_without_p_tags[i:i + chunk_size] for i in
-              range(0, len(message_without_p_tags), chunk_size)]
+        # Split the message into smaller chunks (max 500 characters each)
+        chunk_size = 500
+        chunks = [message_without_p_tags[i:i + chunk_size] for i in range(0, len(message_without_p_tags), chunk_size)]
 
-    translated_chunks = []
-    for chunk in chunks:
-        try:
-            translated_chunk = translator.translate(chunk)
-            translated_chunks.append(translated_chunk)
-        except StopIteration:
-            break
+        translated_chunks = []
+        for chunk in chunks:
+            translated_chunks.append(translator.translate(chunk))
 
-    # Restore the <p> tags in the translated message
-    translated_message = ''.join(translated_chunks).replace('<p_placeholder>', '<p>')
+        # Restore the <p> tags in the translated message
+        translated_message = ''.join(translated_chunks).replace('<p_placeholder>', '<p>')
 
-    # Reinsert emojis back into the translated message
-    for i, emoji_char in enumerate(emojis):
-        translated_message = translated_message.replace(f'<emoji_placeholder_{i}>', emoji_char)
+        # Reinsert emojis back into the translated message
+        for i, emoji_char in enumerate(emojis):
+            translated_message = translated_message.replace(f'<emoji_placeholder_{i}>', emoji_char)
 
-    # Replace the placeholders with the original links
-    for i, link in enumerate(links):
-        translated_message = translated_message.replace(f'<link_placeholder_{i}>', link)
+        # Replace the placeholders with the original links
+        for i, link in enumerate(links):
+            translated_message = translated_message.replace(f'<link_placeholder_{i}>', link)
 
-    # Add a line dynamically informing about the languages being translated
-    info_line = f"Translating message from {source_language} to {target_language}:"
-    translated_message = f"{info_line}\n{translated_message}"
+        # Add a line dynamically informing about the languages being translated
+        info_line = f"Translating message from {source_language} to {target_language}:"
+        translated_message = f"{info_line}\n{translated_message}"
+
+    except Exception as e:
+        # If translation fails, post the original message
+        print(f"Translation Error: {e}")
+        translated_message = f"Message cannot be translated. Original message:\n{message}"
 
     return translated_message
